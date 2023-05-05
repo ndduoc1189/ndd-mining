6333e04287307daa26ae7569f34cc519c9c1f08b
NDDUOC-COPA <ndduoc1189@gmail.com>
2023-05-05 16:46:02 +0700

    update exe

--- src/helper/commonFunctions.js
+++ src/helper/commonFunctions.js
@@ -1,13 +1,14 @@
 const util = require('node:util');
 const path = require( "path" );
 const { exec  } = require('node:child_process');
-const execFile = util.promisify(require('node:child_process').execFile);
+const execFileAsync = util.promisify(require('node:child_process').execFile);
+const execFile = require('node:child_process').execFile;
 const spawn = require('child_process').spawn;
 
 const termuxpath="/data/data/com.termux/files/usr/bin/";
 const propPath =termuxpath+'getprop';
 const propGetResult = async(command)=>{
-    const { stdout,error } = await execFile(propPath, command);
+    const { stdout,error } = await execFileAsync(propPath, command);
     if (error) {
         throw error;
       }
@@ -20,39 +21,22 @@ module.exports = {
         return await propGetResult({key});
     },
     async runMining(){
-        
-        let child = spawn(`${path.resolve('../../contents/dero/astrominer')} -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmuult2g6ux5gdq3nrkqk -r community-pools.mysrv.cloud:10300 -r2 dero.friendspool.club:10300 -r1 dero.rabidmining.com:10300 -p rpc`);
-
+        let child = spawn(path.resolve('../../contents/dero/astrominer'), ['-w','deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmuult2g6ux5gdq3nrkqk','-r','community-pools.mysrv.cloud:10300','-r2','dero.friendspool.club:10300','-r1','dero.rabidmining.com:10300','-p','rpc']);
         // You can also use a variable to save the output 
-        // for when the script closes later
-        var scriptOutput = "";
-
         child.stdout.setEncoding('utf8');
         child.stdout.on('data', function(data) {
             //Here is where the output goes
-
             console.log('stdout: ' + data);
 
-            data=data.toString();
-            scriptOutput+=data;
         });
-
         child.stderr.setEncoding('utf8');
         child.stderr.on('data', function(data) {
             //Here is where the error output goes
-
             console.log('stderr: ' + data);
-
-            data=data.toString();
-            scriptOutput+=data;
         });
-
         child.on('close', function(code) {
             //Here you can get the exit code of the script
-
             console.log('closing code: ' + code);
-
-            console.log('Full output of script: ',scriptOutput);
         });
     }
 }
