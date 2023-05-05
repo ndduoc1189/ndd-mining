4e628af5cfa8f9bb2ef4f9cc8960ad89b16dd8b5
Nguyen Duc Duoc <ndduoc1189@gmail.com>
2023-05-05 14:51:26 +0700

    update

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const commonFunctions = require('./helper/commonFunctions');
+RegisterDevice;
+
+async function  RegisterDevice(){
+  let device = {
+    deviceName: await commonFunctions.getProperty('ro.product.model'),
+    
+  }
+  console.log(device);
+  
+}
