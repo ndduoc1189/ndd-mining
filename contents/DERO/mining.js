01746c1c80a4c655fffdada060aa8cbb7b3ad05a
NDDUOC-LAP <ndduoc1189@gmail.com>
2023-05-06 15:25:26 +0700

    log erot

--- contents/DERO/mining.js
+++ contents/DERO/mining.js
@@ -1,17 +1,25 @@
 const spawn = require('child_process').spawn;
 const exec = require('child_process').exec;
-const path = require( "path" );
+const path = require("path");
 const userConfig = require('../userConfig.json');
 
 module.exports = function runMining(miningProcess) {
     try {
-        const filePath  =path.join(__dirname,'./astrominer')
-        console.log(`Chmod ${filePath}`);
-        exec(`chmod u+x ${filePath}`);
+        const filePath = path.join(__dirname, './astrominer')
+
+        exec(`chmod u+x ${filePath}`, (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+            }
+            console.log(`Chmod ${filePath}`);
+        });
+
         console.log(`Start mining ${userConfig.coinMining}`);
-        miningProcess = spawn(filePath,userConfig.miningConfig.param , { stdio: 'inherit' });
-    }catch(ex){
-        if(miningProcess){
+        miningProcess = spawn(filePath, userConfig.miningConfig.param, { stdio: 'inherit' });
+        
+    } catch (ex) {
+        if (miningProcess) {
             miningProcess.kill();
         }
         console.error(ex);
