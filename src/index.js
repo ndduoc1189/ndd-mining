f8aa2ba90fb605b5fe4dff7524ddc01d3f7c4c71
ndduoc-lap <ndduoc1189@gmail.com>
2023-05-13 01:19:05 +0700

    update

--- src/index.js
+++ src/index.js
@@ -20,18 +20,17 @@ process.on('SIGINT', () => {
 async function runMining(){
   try{
     userConfig = await commonFunctions.getUserConfig();
-    console.log(userConfig);
-    // const miningPath = path.join(__dirname, `../contents/${userConfig.coinMining}/mining.js`);
-    // fs.access(miningPath, fs.F_OK, (err) => {
-    //   if (err) {
-    //     console.error(miningPath+' does not exist');
-    //     console.error(err)
-    //     return
-    //   }
-    // });
-  
-    // const mining = require(miningPath);
-    // mining(miningProcess,userConfig);
+    const miningPath = path.join(__dirname, `../contents/${userConfig.coinMining}/mining.js`);
+    fs.access(miningPath, fs.F_OK, (err) => {
+      if (err) {
+        console.error(miningPath+' does not exist');
+        console.error(err)
+        return
+      }
+    });
+
+    const mining = require(miningPath);
+    mining(miningProcess,userConfig);
 
   }catch (err) {
     console.error(err);
