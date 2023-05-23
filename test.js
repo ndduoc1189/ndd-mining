65f4b03b68473baea40edb98558feff89e2918bf
ndduoc-lap <ndduoc1189@gmail.com>
2023-05-24 00:18:27 +0700

    update 2

--- test.js
+++ test.js
@@ -1,43 +1,28 @@
-const si = require('systeminformation');
+const { exec } = require('child_process');
 
-async function getCPUInfo() {
-  try {
-    // Lấy thông tin CPU
-    const system = await si.system();
-    console.log('system:', system);
-    const cpuInfo = await si.cpu();
-    console.log('Number of CPU Cores:', cpuInfo);
-    const chassis = await si.chassis();
-    console.log('chassis:', chassis);
-    const baseboard = await si.baseboard();
-    console.log('baseboard:', baseboard);
-    const bios = await si.bios();
-    console.log('baseboard:', bios);
-    //Lấy thông tin tải CPU
-    const currentLoad = await si.currentLoad();
-    console.log(currentLoad.currentload)
-    if (typeof currentLoad.currentload !== 'undefined') {
-      console.log('CPU Usage:', currentLoad.currentload.toFixed(2), '%');
-    }
-    
-    // Lấy thông tin nhiệt độ CPU
-    const sensors = await si.cpuTemperature();
-    console.log(sensors)
-    if (typeof sensors.main !== 'undefined') {
-      const temperature = sensors.main;
-      console.log('CPU Temperature:', temperature.toFixed(2), '°C');
-    }
-    
-    // Lấy thông tin điện áp tiêu thụ CPU
-    const voltage = await si.cpuCurrentSpeed();
-    console.log(voltage)
-    if (typeof voltage.voltcore !== 'undefined') {
-      console.log('CPU Voltage:', voltage.voltcore.toFixed(2), 'V');
-    }
-  } catch (error) {
-    console.error('Error:', error);
-  }
+function getCPUVoltage() {
+  return new Promise((resolve, reject) => {
+    exec('termux-battery-status', (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
+        return;
+      }
+      try {
+        const batteryStatus = JSON.parse(stdout);
+        const voltage = batteryStatus.voltage;
+        resolve(voltage);
+      } catch (parseError) {
+        reject(parseError);
+      }
+    });
+  });
 }
 
-// Gọi hàm lấy thông tin CPU
-getCPUInfo();
+// Gọi hàm lấy thông tin CPU voltage
+getCPUVoltage()
+  .then(voltage => {
+    console.log('CPU Voltage:', voltage);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
