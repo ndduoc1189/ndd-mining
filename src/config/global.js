c974df20cc1efffad13a82ae637d9b416932e774
NDDUOC-LAP <ndduoc1189@gmail.com>
2023-06-16 23:39:12 +0700

    update

--- src/config/global.js
+++ src/config/global.js
@@ -1,8 +1,8 @@
 const path = require('path');
 module.exports = {
     binPath :'/data/data/com.termux/files/usr/bin/',
-    apiURL:'http://ws.hhkorean.com/v1',
-    wsURL:'ws://ws.hhkorean.com',
+    apiURL:'https://mnapi.hhkorean.com/v1',
+    wsURL:'wss://mnapi.hhkorean.com',
     configPath: path.join(__dirname,'../../contents/userconfig.json'),
     deviceConfig: path.join(__dirname,'../../contents/deviceconfig.json'),
     logFile : path.join(__dirname,'../../contents/log-mining.txt'),
